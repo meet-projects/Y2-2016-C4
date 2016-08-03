6a0628db46c7818692a7f3a5bbb0937e075a2579
Student <eilon246810@gmail.com>
2016-08-03 13:30:47 +0300

    database.py, initialize.py changed

--- initialize.py
+++ initialize.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-from database_setup import Base, Question, Answer
+from database_setup import Base, Question, Answer, QuestsAndPics
 
 engine = create_engine('sqlite:///project.db')
 Base.metadata.create_all(engine)
@@ -12,10 +12,16 @@ session = DBSession()
 
 # You can add some starter data for your database here.
 
+# This deletes everything in your database.
+session.query(Question).delete()
+session.query(Answer).delete()
+
 ###############################################
 #   MAJD YOU WORK ON THIS   #
+
+
 q1 = Question(
-    #pic_id = 1,
+    pic_id = 1,
     text = 'where do you think picture nom.1 was taken ?',
     a1 = 'Israel',
     a2 = 'USA',
@@ -25,6 +31,7 @@ q1 = Question(
 
 
 q2 = Question(
+	pic_id = 2,
 	text = 'where do you think picture nom.2 was taken ?',
 	a1 = 'Israel',
 	a2 = 'USA',
@@ -33,13 +40,16 @@ q2 = Question(
 	a5 = 'other')
 
 q3 = Question(
+	pic_id = 3,
 	text= 'How easy for you to recognize which one belongs to Palestine and which one belongs to Israel ?',
 	a1 = 'very easy',
 	a2 = 'easy',
 	a3 = 'normal',
 	a4 = 'hard',
 	a5 = 'very hard')
+
 q4 = Question(
+	pic_id = 4,
 	text = 'why ?',
 	a1 = 'its obvious',
 	a2 = 'i have been there',
@@ -47,19 +57,25 @@ q4 = Question(
 	a4 = 'other',
 	a5 = 'something')
 
+x = QuestsAndPics(
+	question_id = q1.id,
+	pic_id = 1)
 
+questions=[q1,q2,q3,q4]
+for question_to_add in questions:
+	session.add(question_to_add)
 ###############################################
+question = session.query(Question).filter_by(id=1).one()
+print (question)
+answer_chosen='a3'
 a1=Answer(
 	question_id = 1,
-    selected='a3',
-    text=session.query(Question).filter_by(id=1).one().a3,
+    selected=answer_chosen,
+    text=getattr(question, answer_chosen),
     nationality = 'Palastinian')
-# This deletes everything in your database.
-session.query(Question).delete()
-session.query(Answer).delete()
-session.commit()
 
-# This adds some rows to the database. Make sure you `commit` after `add`ing!
-session.add(q1)
-session.add(a1)
+answers=[a1]
+for question_to_add in questions:
+	session.add(question_to_add)
+##############################################
 session.commit()
