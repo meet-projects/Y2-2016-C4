ea352254bd915925c91702ed327586b9d5ca5597
Student <lilybreakout11@hotmail.com>
2016-08-09 15:16:57 +0300

    Final + add_answer

---
+++ add_answer.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from random import randint, choice
+
+from database_setup import Base, Answer
+
+
+engine = create_engine('sqlite:///project.db')
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+nats=['Palestinian', 'Israeli', 'Other']
+
+for i in xrange(1,10000):
+	a1=Answer(
+		pair_id = randint(1,20),
+		question_id = randint(1,5),
+	    selected=randint(1,5),
+	    nationality = choice(nats))
+
+	session.add(a1)
+
+
+session.commit()
